bfde1c24e2a673ee75ae92d13c169c091e1abccd
Phan Dang Khoa <dangkhoavtnt223@gmail.com>
2023-04-18 21:18:19 +0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +8,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://rice-software.xyz",
   output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [
     mdx(),
     sitemap(),
